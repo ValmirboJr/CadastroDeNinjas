08f98c461deaedc4f32a29738b05c1465ed4adcf
ValmirboJr <valmirborgesojr@gmail.com>
2025-08-09 17:06:53 -0300

    Implementando TableNinja

---
+++ src/main/java/org/example/cadastrodeninjas/controller/NinjaController.java
@@ -0,0 +1,13 @@
+package org.example.cadastrodeninjas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/Cadastro")
+public class NinjaController {
+
+
+    
+}
---
+++ src/main/java/org/example/cadastrodeninjas/model/Ninja.java
@@ -0,0 +1,27 @@
+package org.example.cadastrodeninjas.model;
+
+import jakarta.persistence.*;
+import org.springframework.context.annotation.Bean;
+
+@Entity
+@Table(name = "tb_Ninja")
+public class Ninja {
+
+    String nome;
+    String email;
+    int idade;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    public Ninja() {
+
+    }
+
+    public Ninja(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+}
+
