ca78a593ce6f244c29ea75600c4389145331a71f
ValmirboJr <valmirborgesojr@gmail.com>
2025-08-18 11:39:29 -0300

    Test: Testando Requisição Via PostMan

--- src/main/java/org/example/cadastrodeninjas/Ninjas/controller/NinjaController.java
+++ src/main/java/org/example/cadastrodeninjas/Ninjas/controller/NinjaController.java
@@ -1,14 +1,38 @@
 package org.example.cadastrodeninjas.Ninjas.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.cadastrodeninjas.Ninjas.model.NinjaModel;
+import org.example.cadastrodeninjas.Ninjas.service.NinjaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/Cadastro")
+@RequestMapping("/cadastro")
 public class NinjaController {
 
 
+    private final NinjaService ninjaService;
 
-    
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @PostMapping("/criar")
+    public NinjaModel criarNinja(@RequestBody NinjaModel ninja) {
+        return ninjaService.CriarNinja(ninja);
+    }
+
+    @PostMapping
+    public List<NinjaModel> listarNinjas(){
+        return ninjaService.ListarNinjas();
+    }
+
+
+    @PostMapping("/listar/{id}")
+    public NinjaModel listarNinjasPorId(@PathVariable long id) {
+        return ninjaService.ListarNinjasComId(id);
+    }
+
 }
