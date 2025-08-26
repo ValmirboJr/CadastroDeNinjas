1850752d8dd508d09ae70dc16a066935112d9c8f
ValmirboJr <valmirborgesojr@gmail.com>
2025-08-25 21:21:32 -0300

    Feat:Implementando Service e Repository

--- src/main/java/org/example/cadastrodeninjas/Missoes/MissoesRepository.java
+++ src/main/java/org/example/cadastrodeninjas/Missoes/MissoesRepository.java
@@ -1,9 +1,15 @@
 package org.example.cadastrodeninjas.Missoes;
 
+import org.example.cadastrodeninjas.Ninjas.NinjaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+    List<MissoesModel> findByAtiva();
+    List<MissoesModel> findByInativa();
 }
--- src/main/java/org/example/cadastrodeninjas/Missoes/MissoesService.java
+++ src/main/java/org/example/cadastrodeninjas/Missoes/MissoesService.java
@@ -4,6 +4,11 @@ import org.example.cadastrodeninjas.Ninjas.NinjaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class MissoesService {
 
@@ -11,9 +16,45 @@ public class MissoesService {
     private MissoesRepository missoesRepository;
 
     @Autowired
-    private NinjaRepository ninjaRepository;
+    private MissoesMapper missoesMapper;
 
-
-
+    public List<MissoesDTO> listarMissoes() {
+        List<MissoesModel>missoes = missoesRepository.findAll();
+        return missoes.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+    public MissoesDTO listarMissaoPorId(Long id) {
+        Optional<MissoesModel> missoesModel = missoesRepository.findById(id);
+        return  missoesModel.map(missoesMapper::map)
+                .orElse(null);
+    }
+    public List<MissoesDTO> listarMissoesAtivas() {
+        return missoesRepository.findByAtiva()
+                .stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+    public List<MissoesDTO> listarMissoesDesativas() {
+        return missoesRepository.findByInativa()
+                .stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+    public MissoesDTO AtivarMissaoPorId(Long id) {
+        Optional<MissoesModel> missoesModel = missoesRepository.findById(id);
+        return missoesModel.map(missoesMapper::map)
+                .orElse(null);
+    }
+    public MissoesDTO DesativaMissoesPorId(Long id) {
+        Optional<MissoesModel> missoesModel = missoesRepository.findById(id);
+        return missoesModel.map(missoesMapper::map)
+                .orElse(null);
+    }
+    public MissoesDTO CriarMissoes(MissoesDTO missoesDTO) {
+        MissoesModel missoesModel = missoesMapper.map(missoesDTO);
+        missoesRepository.save(missoesModel);
+        return missoesMapper.map(missoesModel);
+    }
 
 }
